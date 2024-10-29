27a3b4f67b4e12c987a159effcffb2c9e3dc0d99
Ina Jobbet <LJM@MUA100-10136.kommun.ljusdal.se>
2024-10-29 11:22:18 +0100

    filtering user inpt data to remove anything mal

--- inc/filter-post-data.php
+++ inc/filter-post-data.php
@@ -2,6 +2,12 @@
 add_filter('wp_insert_post_data', 'filter_data_custom');
 
 function filter_data_custom($data) {
+    // sanitize incoming text to make shure no malicius code enters the database
+    if ($data['post_type'] == 'ljm_note') {
+        $data['post_content'] = sanitize_textarea_field($data['post_content']);
+        $data['post_title'] = sanitize_text_field($data['post_title']);
+    }
+    //changes status to private so that it cant be accesed without login
     if ($data['post_type'] == 'ljm_note' && $data['post_status'] != 'trash') {
         $data['post_status'] = 'private';
     }
